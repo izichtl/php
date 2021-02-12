4eae984b54c9f026af63f6d4885da3113cfab628
izichtl <izichtl@gmail.com>
2021-02-12 15:46:25 -0300

    post_data

--- formularioPhp/index.php
+++ formularioPhp/index.php
@@ -1,41 +1,16 @@
-<?php 
-
-
-
-
-$categorias = [];
-$categorias[] = 'Infantil';
-$categorias[] = 'Adolescente';
-$categorias[] = 'Adulto';
-
-$nome = 'Eduardo';
-$idade = 20;
-
-
-if($idade >= 6 && $idade <= 12){
-    for($i = 0; $i <= count($categorias)-1; $i++){
-        if($categorias[$i] == 'Infantil'){
-            echo 'O nadador ', $nome, ' está na categoria ', $categorias[$i];
-        }
-    }
-} 
-else if($idade >= 13 && $idade <=18){
-    for($i = 0; $i <= count($categorias)-1; $i++){
-        if($categorias[$i] == 'Adolescente'){
-            echo 'O nadador ', $nome, ' está na categoria ', $categorias[$i];
-        }
-    }
-}
-else{
-    for($i = 0; $i <= count($categorias)-1 ; $i++){
-        if($categorias[$i] == 'Adulto'){
-            echo 'O nadador ', $nome, ' está na categoria ', $categorias[$i];
-        }
-    }
-    
-}
-
-
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de inscrição</title>
+</head>
+<body>
+    <h2>Formulário para inscrição de competidores</h2>
+    <form action='script.php' method='post'>
+        <p>Seu nome: <input type='text' name='nome'></p>
+        <p>Sua idade: <input type='text' name='idade'></p>
+        <p><input type='submit' value='Inscrever'></p>
+</body>
+</html>
